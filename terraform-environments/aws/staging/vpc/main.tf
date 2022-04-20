10dfa50b24c0a9f4e5e753cb026eb0af3e80767a
usmanlakhani <usmanlakhani@hotmail.com>
2022-04-20 14:50:28 -0400

    Fixed AZ code

--- terraform-environments/aws/staging/vpc/main.tf
+++ terraform-environments/aws/staging/vpc/main.tf
@@ -39,7 +39,7 @@ module "vpc" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.30"
   #source = "./terraform-modules/aws/vpc"
   aws_region       = local.aws_region
-  azs              = ["ca-central-1a", "ca-central-1b", "uca-central-1d"]
+  azs              = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
   vpc_cidr         = "10.0.0.0/16"
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
